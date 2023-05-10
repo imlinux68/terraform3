9dc0661626007e9d26a1f1ffabd34e1f5ced3e83
im linux <imlinux68@gmail.com>
2023-05-10 20:30:48 +0300

    added backend in s3

--- provider.tf
+++ provider.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "us-west-2"
 }
 
+terraform {
+    backend "s3" {
+      bucket = "myterraform.10.05.2023"
+      key = "terraform.tfstate"
+      region = "us-west-2"
+    }
+}
+
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
   instance_tenancy = "default"
